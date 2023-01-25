c9c95b38c4b086a675a690abf753a9b3c2f9ea0d
abiyyu03 <abiyyu652@gmail.com>
2023-01-25 22:43:39 +0700

    fix livecount

--- public/js/livecount/livecount.js
+++ public/js/livecount/livecount.js
@@ -1,43 +1,10 @@
 const canvasElement = document.getElementById('chartVotingSementara');
-const chartVotingSementara = new Chart(canvasElement, {
-    type: 'bar',
-    data: {
-        labels: '',
-        datasets: [{
-            label: 'Hasil Vote',
-            data: '',
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-            ],
-            hoverOffset: 4
-        }]
-    },
-    options: {
-        responsive:true,
-        plugins : {
-            datalabels:{
-                formatter:(value,live_count) => {
-                    let sum = 0;
-                    const dataArray = live_count.chart.data.datasets[0].data;
-                    dataArray.map(data => {
-                        sum += parseInt(data);
-                    });
-                    let percentage = (value * 100 / sum).toFixed(2)+"%";
-                    return percentage;
-                },
-                color:'#fff',
-                backgroundColor:'#000'
-            },
-        }
-    },
-});
 
-const liveCountChart = () => {
+
+// const liveCountChart = () => {
     $.ajax({
         url:'http://0.0.0.0:8000/api/live_count',
         type:'GET',
-        cache:false,
         dataType:'json',
         success: function(response){
             let kandidatData = response.data;
@@ -49,22 +16,56 @@ const liveCountChart = () => {
                 dataJumlahSuara.push(response.jumlah_suara);
                 namaKandidat.push(response.nama_kandidat);
             });
+
+            new Chart(canvasElement, {
+                type: 'bar',
+                data: {
+                    labels: namaKandidat,
+                    datasets: [{
+                        label: 'Hasil Vote',
+                        data: dataJumlahSuara,
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                        ],
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
+                    responsive:true,
+                    plugins : {
+                        datalabels:{
+                            formatter:(value,live_count) => {
+                                let sum = 0;
+                                const dataArray = live_count.chart.data.datasets[0].data;
+                                dataArray.map(data => {
+                                    sum += parseInt(data);
+                                });
+                                let percentage = (value * 100 / sum).toFixed(2)+"%";
+                                return percentage;
+                            },
+                            color:'#fff',
+                            backgroundColor:'#000'
+                        },
+                    }
+                },
+            });
             // let dataJumlahSuara = kandidat.length == 0 ? [0, 0] : [kandidatData[0].jumlah_suara, kandidatData[1].jumlah_suara];
-            chartVotingSementara.data.labels = namaKandidat;
-            chartVotingSementara.data.datasets[0].data = dataJumlahSuara;
-            chartVotingSementara.update();
-            alert(chartVotingSementara.data.datasets[0].data)
+            // chartVotingSementara.data.labels = namaKandidat;
+            // chartVotingSementara.data.datasets[0].data = dataJumlahSuara;
+            // chartVotingSementara.update();
+            // alert(chartVotingSementara.data.datasets[0].data)
         }
     });
-}
-
-let i = 1;
-const countDown = () => {
-    console.log(i);
-    document.getElementById('countDown').textContent = i--;
-    if(i === 0) {
-        i = 20;
-        liveCountChart();
-    };
-}
-setInterval(()=> {alert('halow')}), 1000);
+// }
+liveCount();
+// let i = 1;
+// const countDown = () => {
+//     console.log(i);
+//     document.getElementById('countDown').textContent = i--;
+//     if(i === 0) {
+//         i = 20;
+//         liveCountChart();
+//     };
+// }
+// setInterval(()=> {alert('halow')}), 1000);
--- resources/views/partials/pages/home/chart-info-voting.blade.php
+++ resources/views/partials/pages/home/chart-info-voting.blade.php
@@ -5,7 +5,7 @@
             <div class="col-lg-8">
                 <div data-aos="fade-down">
                     <h1 id="count" class="fw-bold text-color mt-5 mb-5 text-center">Perhitungan Langsung Sementara</h1>
-                    <p class="bg-primary badge rounded p-2 text-center">Data akan diupdate dalam : <span id="countDown"></span></p>
+                    {{-- <p class="bg-primary badge rounded p-2 text-center">Data akan diupdate dalam : <span id="countDown"></span></p> --}}
                     <canvas id="chartVotingSementara" aria-label="chart voting sementara"></canvas>
                 </div>
             </div>
