746312c5d8744ec02f19eb0055cb26f022c7e6b5
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2023-01-31 13:49:32 +0700

    change color admin

--- resources/views/pages/admin/voting.blade.php
+++ resources/views/pages/admin/voting.blade.php
@@ -13,20 +13,20 @@ active
 
 @section('content')
 <div class="container-fluid">
-    
+
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Voting</h1>
     </div>
-    
+
     <div class="row">
-        
+
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-4">
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Jumlah Suara Masuk</h6>
+                    <h6 class="m-0 fnt-kandidat">Jumlah Suara Masuk</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -44,7 +44,7 @@ active
                         <tbody>
 
                         </tbody>
-                    </table> 
+                    </table>
                 </div>
             </div>
         </div>
@@ -52,7 +52,7 @@ active
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Data Suara Masuk</h6>
+                    <h6 class="m-0 fnt-kandidat">Data Suara Masuk</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -77,33 +77,35 @@ active
                 </div>
             </div>
         </div>
-        
+
     </div>
     @endsection
-    
+
     @section('dataTablesJS')
     <script src="{{ asset('template/vendor/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('template/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('template/js/demo/datatables-demo.js') }}"></script>
     <script src="{{asset('js/livecount/livecount.js')}}"></script>
     <script>
-        $.ajax({
-        url:'http://127.0.0.1:8000/api/live_count',
-        type:'GET',
-        dataType:'json',
-        success: function(response){
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/live_count',
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
             let kandidatData = response.data;
             // let dataJumlahSuara = [];
             // let namaKandidat = [];
 
             kandidatData.forEach(response => {
-                $('#myTable tr:last').after(`<tr><td>${response.id}</td><td>${response.nama_kandidat}</td><td>${response.jumlah_suara}</td></tr>`);
+                $('#myTable tr:last').after(
+                    `<tr><td>${response.id}</td><td>${response.nama_kandidat}</td><td>${response.jumlah_suara}</td></tr>`
+                );
             });
         }
-        });
+    });
     </script>
     @endsection
-    
+
     {{-- @push('scripts')
    
     @endpush --}}
