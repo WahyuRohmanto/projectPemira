421b200148066d90285461354939abe99f661cef
abiyyu03 <abiyyu652@gmail.com>
2023-02-04 13:03:59 +0700

    fix assets

--- public/js/totalsuara/totalsuara.js
+++ public/js/totalsuara/totalsuara.js
@@ -1,15 +1,12 @@
-$.ajax({
-    url: 'http://127.0.0.1:8000/api/live_count',
-    type: 'GET',
-    dataType: 'json',
-    cache:true,
-    success: function(response) {
-        let kandidatData = response.data;
+const countUserHasVote = async () => {
+    const response = await axios.get('/api/live_count');
+    let kandidatData = response.data.data;
 
-        kandidatData.forEach(response => {
-            $('#myTable tr:last').after(
-                `<tr><td>${response.id}</td><td>${response.nama_kandidat}</td><td>${response.jumlah_suara}</td></tr>`
-            );
-        });
-    }
-});
+    kandidatData.forEach(response => {
+        $('#myTable tr:last').after(
+            `<tr><td>${response.id}</td><td>${response.nama_kandidat}</td><td>${response.jumlah_suara}</td></tr>`
+        );
+    });
+    
+}
+countUserHasVote();
--- resources/views/pages/admin/voting.blade.php
+++ resources/views/pages/admin/voting.blade.php
@@ -13,14 +13,14 @@ active
 
 @section('content')
 <div class="container-fluid">
-
+    
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Voting</h1>
     </div>
-
+    
     <div class="row">
-
+        
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-4">
             <div class="card shadow mb-4">
@@ -33,7 +33,7 @@ active
                     <div class="chart-pie pt-4 pb-1">
                         <canvas id="chartVotingSementara" aria-label="chart voting sementara"></canvas>
                     </div>
-                    <table class="table table-bordered table-striped" id="myTable">
+                    <table class="table table-bordered table-striped mt-3" id="myTable">
                         <thead>
                             <tr>
                                 <th>#</th>
@@ -42,7 +42,7 @@ active
                             </tr>
                         </thead>
                         <tbody>
-
+                            
                         </tbody>
                     </table>
                 </div>
@@ -80,14 +80,22 @@ active
                 </div>
             </div>
         </div>
-
+        
     </div>
     @endsection
-
+    
     @section('dataTablesJS')
     <script src="{{ asset('template/vendor/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('template/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('template/js/demo/datatables-demo.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"
+        integrity="sha512-60KwWtZOhzgr840mc57MV8JqDZHAws3w61mhK45KsYHmhyNFJKmfg4M7/s2Jsn4PgtQ4Uhr9xItS+HCbGTIRYQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.25.0/axios.min.js"
+        integrity="sha512-/Q6t3CASm04EliI1QyIDAA/nDo9R8FQ/BULoUFyN4n/BDdyIxeH7u++Z+eobdmr11gG5D/6nPFyDlnisDwhpYA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{asset('js/livecount/livecount.min.js')}}"></script>
     <script src="{{asset('js/totalsuara/totalsuara.js')}}"></script>
     @endsection
+    @push('kode')
+    @endpush
